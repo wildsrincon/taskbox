28d9835dc9d25cf10e15d40ce2a46025f92541cd
Wilder Rincon <1588@holbertonschool.com>
2022-02-08 12:41:47 -0500

    Adding screen task storybook tutorial

---
+++ src/lib/store.js
@@ -0,0 +1,69 @@
+/* A simple redux store/actions/reducer implementation.
+ * A true app would be more complex and separated into different files.
+ */
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+// Our new error field is configured here
+const AppStateSlice = createSlice({
+    name: "appState",
+    initialState: "",
+    reducers: {
+        updateAppState: (state, action) => {
+            return {
+                ...state,
+                isError: action.payload,
+            };
+        },
+    },
+});
+
+/*
+ * The initial state of our store when the app loads.
+ * Usually, you would fetch this from a server.
+ */
+const defaultTasks = [
+  { id: '1', title: 'Something', state: 'TASK_INBOX' },
+  { id: '2', title: 'Something more', state: 'TASK_INBOX' },
+  { id: '3', title: 'Something else', state: 'TASK_INBOX' },
+  { id: '4', title: 'Something again', state: 'TASK_INBOX' },
+];
+
+/*
+ * The store is created here.
+ * You can read more about Redux Toolkit's slices in the docs:
+ * https://redux-toolkit.js.org/api/createSlice
+ */
+const TasksSlice = createSlice({
+  name: 'tasks',
+  initialState: defaultTasks,
+  reducers: {
+    updateTaskState: (state, action) => {
+      const { id, newTaskState } = action.payload;
+      const task = state.findIndex(task => task.id === id);
+      if (task >= 0) {
+        state[task].state = newTaskState;
+      }
+    },
+  },
+});
+
+
+// The actions contained in the slice are exported for usage in our components
+export const { updateTaskState } = TasksSlice.actions;
+
+// The actions contained in the new slice are exported to be used in our components
+export const { updateAppState } = AppStateSlice.actions;
+
+/*
+ * Our app's store configuration goes here.
+ * Read more about Redux's configureStore in the docs:
+ * https://redux-toolkit.js.org/api/configureStore
+ */
+const store = configureStore({
+  reducer: {
+    tasks: TasksSlice.reducer,
+    isError: AppStateSlice.reducer,
+  },
+});
+
+export default store;
